626e6e1b70a39e4292279e699e7de7327303cf62
Sanja Jasek <jasek.sanja@gmail.com>
2016-06-08 19:08:49 +0200

    Using different dictionary structure for counting reads for reference.

--- k-mer_mapper.py
+++ k-mer_mapper.py
@@ -22,14 +22,11 @@ for read in SeqIO.parse(ReadsFile, "fasta"):
 	ReadSequence=str(read.seq)
 	length=len(ReadSequence)
 	length=length-15
-	print(length)
 	for num in range(0,length):
 		first=num
 		last=num+15
 		kmer=ReadSequence[first:last]
-		print(kmer)
 		hash[kmer]=hash.get(kmer,0)+1
-print(hash)
 
 ReadsFile.close()
 
@@ -43,31 +40,25 @@ RefFile=open(RefFileName,'r')
 refseqs={}
 
 for seq_record in SeqIO.parse(RefFile, "fasta"):
-	#print(seq_record.id)
+
 	header=seq_record.id
-	#print(seq_record.seq)
+
 	seq=str(seq_record.seq)
 	#print(hash.keys())
 	for hashkey in hash.keys():
-		print(hashkey)
+		#print(hashkey)
 		if hashkey in seq:
 			#print(hash[hashkey])
-			#print('printing')
 			#print(refseqs.get(seq))
-			adding=0
-			if seq in refseqs:
-				adding=refseqs[seq][1]
-				print(str(adding)+" alreaddy mapped")
-			refseqs[seq]=[header, adding + hash[hashkey]]
-			print(str(hash[hashkey]) +" is new hash to add")
-			print(refseqs[seq])
+			refseqs[header]=refseqs.get(header,0)+hash[hashkey]
+			#print(str(hash[hashkey]) +" is new hash to add")
+		else:
+			refseqs[header]=refseqs.get(header,0)
+	print(str(refseqs[header])+" k-mers map to "+header)
 
-	#refseqs[sequence][0] -> to access readnumber
+	#refseqs[sequence][0] -> to access first number in list with key "sequence"
 
 
-
-
-#make seq key, make header as first element in list, number of times it appears second element
 
 
 #print(refseqs)
