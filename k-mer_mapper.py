1c3dce48686da9dab0467de640db7c6008686764
Sanja Jasek <jasek.sanja@gmail.com>
2016-06-08 17:48:04 +0200

    rewrote k-merization to use biopython

--- k-mer_mapper.py
+++ k-mer_mapper.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import sys
+from Bio import SeqIO
 
-'''
+
 # step 1: open reads file:
 
 arguments = sys.argv
@@ -17,38 +18,48 @@ ReadsFile=open(ReadsFileName,'r')
 
 hash={}
 
-for Line in ReadsFile:
-	Line=Line.strip() #remove whitespace in beggining and end, because print again adds line endings
-	if not '>' in Line:
-		length=len(Line)
-		length=length-15
-		print(length)
-		for num in range(0,length):
-			first=num
-			last=num+15
-			kmer=Line[first:last]
-			print(kmer)
-			hash[kmer]=hash.get(kmer,0)+1
-			
-
+for read in SeqIO.parse(ReadsFile, "fasta"):
+	ReadSequence=str(read.seq)
+	length=len(ReadSequence)
+	length=length-15
+	print(length)
+	for num in range(0,length):
+		first=num
+		last=num+15
+		kmer=ReadSequence[first:last]
+		print(kmer)
+		hash[kmer]=hash.get(kmer,0)+1
 print(hash)
 
 ReadsFile.close()
-'''
+
 # step 3: search reference with k-mers, each match gets k-mer value added to the sequence
 # open reference
-# remove newlines in fasta
+'''
 RefFileName=sys.argv[2]
 
 RefFile=open(RefFileName,'r')
 
-for Line in RefFile:
-	Line=Line.strip() #remove whitespace in beggining and end, because print again adds line endings
-	print(Line)
+refseqs={}
+sequence=''
+
+for seq_record in SeqIO.parse(RefFile, "fasta"):
+    print(seq_record.id)
+    print(seq_record.seq)
+
+
+
+
+#make seq key, make header as firs element in list, number of times it appears second element
+
+
+print(refseqs)
+
+
 RefFile.close()
 
 
-
+'''
 
 
 
