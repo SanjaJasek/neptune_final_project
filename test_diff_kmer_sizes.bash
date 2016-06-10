075bfd4dbf36bd5f6df9371b7939ff28be17c496
Sanja Jasek <jasek.sanja@gmail.com>
2016-06-10 14:24:33 +0200

    added bash script to test how k-mer length affects mapping

---
+++ test_diff_kmer_sizes.bash
@@ -0,0 +1,78 @@
+#!/bin/bash -x
+
+mapper="./k-mer_mapper.py"
+reads="cow_mRNA_reads.fasta"
+ref="cow_mRNA.fasta"
+
+# collect statistics for no mask
+nomaskHs=()
+nomaskks=()
+nomaskrs=()
+nomaskout="nomask_stats.txt"
+
+# collect statistics for mask
+maskHs=()
+maskks=()
+maskrs=()
+nomaskout="mask_stats.txt"
+
+# write headers:
+header () {
+fil=$1
+echo "k-mer size\tmean Shannon entorpy\tmean number of k-mers per sequence\tmean number of mapped reads per sequence" > $fil
+}
+
+header $nomaskout
+header $maskout
+
+stats () {
+stfil=$1
+string=$2
+
+value=$( grep "$dtring" "$stfil" )
+value=${value##*: }
+
+return $value
+#echo $value
+}
+
+for thr in 5 10 15 20 25 30
+do 
+	# map reads without mask
+	nomask="cow_mRNA_reads_${thr}_statistics.txt"
+
+	[[ -e $nomask ]] || $mapper -reads $reads -ref $ref -k $thr
+
+	# get stats
+	
+	nomaskH=$(stats $nomask "Shannon entropy")
+	nomaskHs+=($nomaskH)
+	
+	nomaskk=$( stats $nomask "k-mers per sequence"  )
+	nomaskks+=($nomaskk)
+
+	nomaskr=$( stats $nomask "k-mers per sequence"  )
+	nomaskrs+=($nomasrk)
+
+	echo "$nomaskH\t$nomaskk\t$nomaskr" >> $nomaskout
+
+	# map reads with mask
+
+	mask="cow_mRNA_reads_${thr}_mask_statistics.txt"
+
+	[[ -e $mask ]] || $mapper -reads $reads -ref $ref -k $thr -m
+
+	# get stats
+	
+	maskH=$(stats $mask "Shannon entropy")
+	maskHs+=($maskH)
+	
+	maskk=$( stats $mask "k-mers per sequence"  )
+	maskks+=($maskk)
+
+	maskr=$( stats $mask "k-mers per sequence"  )
+	masrks+=($masrk)
+
+	echo "$maskH\t$maskk\t$maskr" >> $maskout
+done
+
